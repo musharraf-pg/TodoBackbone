77d186a65446d28196f941fdcfe7f3413e5ff9a6
Musharraf Azeem <musharraf.azeem@prodigygame.com>
2017-11-24 18:38:26 -0500

    Create and delete functionality

--- app.js
+++ app.js
@@ -26,9 +26,12 @@ app.Views.TodoAppView = Backbone.View.extend({
             {title: "CCC"}
         ]);
         this.todoListView = new app.Views.ToDoListView({collection: todoItemsCollection});
-        this.subviews = [todoListView];
+        this.subviews = [this.todoListView];
 
         this.render();
+    },
+    events: {
+        "keypress .js-new-todo": "createTodoOnEnter"
     },
     template: Handlebars.compile($("#todo-app-template").html()),
     render: function() {
@@ -43,25 +46,40 @@ app.Views.TodoAppView = Backbone.View.extend({
         this.todoContent.append(this.todoListView.render());
 
         return this.$el;
+    },
+    createTodoOnEnter: function(e) {
+        const title = this.newTodoField.val().trim();
+        if (e.which == 13 && title != "") {
+
+            this.todoListView.collection.add(new app.Models.TodoItem({
+                title: title
+            }));
+        }
     }
 });
 app.Views.ToDoItemView = Backbone.View.extend({
     tagName: "li",
     className: "todo-item",
+    events: {
+        "click .js-delete-btn": "delete"
+    },
     template: Handlebars.compile($("#todo-item-template").html()),
     render: function() {
         console.log("rendering item view: ", this.model);
 
         this.$el.html(this.template(this.model.toJSON()))
         return this.$el;
+    },
+    delete: function() {
+        this.model.destroy();
     }
 });
 app.Views.ToDoListView = Backbone.View.extend({
     tagName: "ul",
     className: ".todo-items",
     initialize: function() {
-        // this.listenTo(this.collection, "add", );
-        // this.listenTo(this.collection, "remove", );
+        this.listenTo(this.collection, "add", this.render);
+        this.listenTo(this.collection, "remove", this.render);
 
         // app.todoItems.fetch();
         this.subviews = [];
